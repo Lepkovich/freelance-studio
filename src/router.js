6f5567408e161297276a8e8b3ad6cac3edfcadd8
Lepkovich <6332133@gmail.com>
2024-05-06 19:41:28 +0300

    part 1 is done. core, webpack, routing and layout

--- src/router.js
+++ src/router.js
@@ -6,40 +6,44 @@ import {SignUp} from "./components/sign-up";
 export class Router {
     constructor() {
         this.titlePageElement = document.getElementById('title');
-        this.contentPageElement = document.getElementById('contend');
+        this.contentPageElement = document.getElementById('content');
 
         this.initEvents();
-            this.routes = [
-                {
-                    route: '/',
-                    title: 'Дашборд',
-                    filePathTemplate: '/templates/dashboard.html',
-                    load: () => {
-                        new Dashboard();
-                    }
-                },
-                {
-                    route: '/404',
-                    title: 'Страница не найдена',
-                    filePathTemplate: '/templates/404.html'
-                },
-                {
-                    route: '/login',
-                    title: 'Авторизация',
-                    filePathTemplate: '/templates/login.html',
-                    load: () => {
-                        new Login();
-                    }
-                },
-                {
-                    route: '/sign-up',
-                    title: 'Регистрация',
-                    filePathTemplate: '/templates/sign-up.html',
-                    load: () => {
-                        new SignUp();
-                    }
-                },
-            ];
+        this.routes = [
+            {
+                route: '/',
+                title: 'Дашборд',
+                filePathTemplate: '/templates/dashboard.html',
+                useLayout: '/templates/layout.html',
+                load: () => {
+                    new Dashboard();
+                }
+            },
+            {
+                route: '/404',
+                title: 'Страница не найдена',
+                filePathTemplate: '/templates/404.html',
+                useLayout: false,
+            },
+            {
+                route: '/login',
+                title: 'Авторизация',
+                filePathTemplate: '/templates/login.html',
+                useLayout: false,
+                load: () => {
+                    new Login();
+                }
+            },
+            {
+                route: '/sign-up',
+                title: 'Регистрация',
+                filePathTemplate: '/templates/sign-up.html',
+                useLayout: false,
+                load: () => {
+                    new SignUp();
+                }
+            },
+        ];
     }
 
     initEvents() {
@@ -57,7 +61,18 @@ export class Router {
             }
 
             if (newRoute.filePathTemplate) {
-                this.contentPageElement.innerHTML = await fetch(newRoute.filePathTemplate).then(response => response.text())
+                let contentBlock = this.contentPageElement;
+                if (newRoute.useLayout) {
+                    this.contentPageElement.innerHTML = await fetch(newRoute.useLayout).then(response => response.text());
+                    contentBlock =  document.getElementById('content-layout');
+                    document.body.classList.add('sidebar-mini');
+                    document.body.classList.add('layout-fixed');
+                }
+                else {
+                    document.body.classList.remove('sidebar-mini');
+                    document.body.classList.remove('layout-fixed');
+                }
+                contentBlock.innerHTML = await fetch(newRoute.filePathTemplate).then(response => response.text())
             }
 
             if (newRoute.load && typeof newRoute.load === 'function') {
